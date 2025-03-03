e77b7a0de28fad52ac95b81fc81c45a093b6656a
smkim <smjy9502@naver.com>
2025-03-03 18:08:59 +0900

    prod_deploy_firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:faber_ticket_ft/screens/main_screen.dart';
 import 'package:faber_ticket_ft/screens/error_screen.dart';
 import 'firebase_options.dart';
-import 'package:flutter/foundation.dart' show kDebugMode, kIsWeb;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -12,11 +11,6 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
-  if (kDebugMode) {
-    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8081);
-    await FirebaseStorage.instance.useStorageEmulator('localhost', 9199);
-  }
 
   runApp(MyApp());
 }
