00e7090259e5d1fd0ae787949fe2e1621c93d561
smkim <smjy9502@naver.com>
2025-03-12 01:17:52 +0900

    nfc-onlyMobile-v3.18

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' as foundation;
-
 import 'package:faber_ticket_ft/screens/main_screen.dart';
 import 'package:faber_ticket_ft/screens/error_screen.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:html' as html;
-import 'dart:ui' as ui;
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -48,23 +44,12 @@ Future<bool> checkInitialAccess() async {
   if (foundation.kIsWeb) {
     final userAgent = html.window.navigator.userAgent;
     final isMobile = userAgent.contains('Mobile');
-
     if (isMobile) {
-      // 모바일 기기에서만 접속 허용
-      return true;
+      return true; // 모바일 기기에서만 접속 허용
     } else {
       return false;
     }
   } else {
-    // 모바일 앱에서는 NFC 기능을 사용하여 접속 제한
-    final prefs = await SharedPreferences.getInstance();
-    bool isFromNFC = prefs.getBool('isFromNFC') ?? false;
-    if (isFromNFC) {
-      await prefs.setBool('isFromNFC', false);
-      print("NFC flag reset");
-      return true;
-    } else {
-      return false;
-    }
+    return true; // 모바일 앱에서는 NFC 기능을 사용하여 접속 제한 (현재는 NFC 관련 코드가 필요하지 않음)
   }
 }
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:faber_ticket_ft/utils/constants.dart';
 import 'package:flutter/material.dart';
@@ -6,9 +5,6 @@ import 'package:faber_ticket_ft/screens/custom_screen.dart';
 import 'package:faber_ticket_ft/widgets/custom_button.dart';
 import 'package:faber_ticket_ft/services/firebase_service.dart';
 import 'error_screen.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:nfc_manager/nfc_manager.dart';
-import 'package:universal_html/html.dart' as html;
 
 class MainScreen extends StatefulWidget {
   @override
@@ -16,63 +12,6 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  final FirebaseService _firebaseService = FirebaseService();
-
-  @override
-  void initState() {
-    super.initState();
-    // checkNFCAccess();
-  }
-
-  Future setNFCFlag() async {
-    if (kIsWeb) {
-      html.window.localStorage['isFromNFC'] = 'true';
-      print("NFC Flag set in Local Storage");
-    } else {
-      final prefs = await SharedPreferences.getInstance();
-      await prefs.setBool('isFromNFC', true);
-      print("NFC Flag set in SharedPreferences");
-    }
-  }
-
-  // main_screen.dart
-  Future checkNFCAccess() async {
-    if (!kIsWeb) { // 모바일 기기에서만 NFC 기능 사용
-      bool isAvailable = await NfcManager.instance.isAvailable();
-      if (isAvailable) {
-        NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async {
-          print("NFC Tag detected!");
-          await setNFCFlag();
-          final prefs = await SharedPreferences.getInstance();
-          bool isFromNFC = prefs.getBool('isFromNFC') ?? false;
-          if (isFromNFC) {
-            print("Access granted, staying on MainScreen");
-            setState(() {});
-          } else {
-            print("Access denied, navigating to ErrorScreen");
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => ErrorScreen()),
-            );
-          }
-        });
-      } else {
-        print('NFC not available');
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => ErrorScreen()),
-        );
-      }
-    } else {
-      // 웹 플랫폼에서는 다른 인증 방법 사용
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => ErrorScreen()),
-      );
-    }
-  }
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
