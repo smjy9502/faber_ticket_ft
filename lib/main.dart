e5e5a926a934bc668e638a5fc950bb1e8f8ee477
smkim <smjy9502@naver.com>
2025-03-12 01:07:25 +0900

    nfc-onlyMobile-v3.16

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' as foundation;
+
 import 'package:faber_ticket_ft/screens/main_screen.dart';
 import 'package:faber_ticket_ft/screens/error_screen.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:html' as html;
+import 'dart:ui' as ui;
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,7 +28,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'CustomFont',
       ),
       home: FutureBuilder(
-        future: checkInitialNFCAccess(),
+        future: checkInitialAccess(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return CircularProgressIndicator();
@@ -39,16 +44,27 @@ class MyApp extends StatelessWidget {
   }
 }
 
-Future<bool> checkInitialNFCAccess() async {
-  final prefs = await SharedPreferences.getInstance();
-  bool isFromNFC = prefs.getBool('isFromNFC') ?? false;
-  print("Initial NFC access check: $isFromNFC");
-  if (isFromNFC) {
-    await prefs.setBool('isFromNFC', false);
-    print("NFC flag reset");
-    return true;
+Future<bool> checkInitialAccess() async {
+  if (foundation.kIsWeb) {
+    final userAgent = html.window.navigator.userAgent;
+    final isMobile = userAgent.contains('Mobile');
+
+    if (isMobile) {
+      // 모바일 기기에서만 접속 허용
+      return true;
+    } else {
+      return false;
+    }
   } else {
-    // NFC칩 없이 접속 시도 시 에러 화면으로 이동
-    return false;
+    // 모바일 앱에서는 NFC 기능을 사용하여 접속 제한
+    final prefs = await SharedPreferences.getInstance();
+    bool isFromNFC = prefs.getBool('isFromNFC') ?? false;
+    if (isFromNFC) {
+      await prefs.setBool('isFromNFC', false);
+      print("NFC flag reset");
+      return true;
+    } else {
+      return false;
+    }
   }
 }
